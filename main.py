4349c432ea4d2f28be3084839c5e999fd9bdb1c3
Abdullah Ansari <abdullah@sysconinfotech.com>
2024-11-28 13:03:05 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,63 @@
+# BASIC PRINT
+
+# print('Hello World'),
+
+# a = input("Enter your name:"),
+# print("Your Name is",a),
+
+# BASIC PRINT
+
+
+
+# BASIC LOOP
+
+# name = 'Abdullah Ansari'
+
+# for character in name:
+#     print(character)
+
+# BASIC LOOP
+
+
+
+
+# STRING METHODS
+
+# name = 'Abdullah'
+
+# print(name.upper())
+
+# STRING METHODS
+
+
+
+
+# IF ELSE CONDITIONS 
+
+# num = input("Enter your age: ")
+
+# if(int(num) >= 18):
+#     print("You can drive")
+# else:
+#     print("You can not drive")
+
+# IF ELSE CONDITIONS 
+
+
+
+
+#  MATCH STATEMENT CONDITIONS
+
+num = int(input('Enter you age: '))
+
+match num:
+    case _ if num > 18:
+        print('You are more then 18')
+    case _ if num < 18:
+        print('You are lees than 18')
+    case _:
+        print('Your age is',num)
+
+#  MATCH STATEMENT CONDITIONS
+
+ 
