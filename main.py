1778aa1cb669b4b8eb4428c671900eaac93aa857
Abdullah Ansari <abdullah@sysconinfotech.com>
2024-11-28 13:34:14 +0530

    functions

--- main.py
+++ main.py
@@ -48,17 +48,37 @@
 
 #  MATCH STATEMENT CONDITIONS
 
-num = int(input('Enter you age: '))
+# num = int(input('Enter you age: '))
 
-match num:
-    case _ if num > 18:
-        print('You are more then 18')
-    case _ if num < 18:
-        print('You are lees than 18')
-    case _:
-        print('Your age is',num)
+# match num:
+#     case _ if num > 18:
+#         print('You are more then 18')
+#     case _ if num < 18:
+#         print('You are lees than 18')
+#     case _:
+#         print('Your age is',num)
 
 #  MATCH STATEMENT CONDITIONS
 
  
+#   FUNCTIONS 
+
+# def average(a,b):
+#  print('Average of a and b plus', (a+b)/2)
  
+# average(3,5)
+
+# def average(*nums):
+#   sum = 0
+#   for i in nums:
+#     sum = sum + i
+#   print('Average ', sum / len(nums))
+
+# average(5*6)  
+
+def names(**names):
+    print(names['fname'],names['mname'],names['lname'])
+
+names(fname='Abdullah',mname='Abdul Vadud',lname='Ansari')
+
+#   FUNCTIONS
